eddcd46e046b3dddc553e8dea33358a3e5504f39
Administrator <Administrator@SUPER-20170704Z>
2018-08-01 16:45:25 +0800

    初始化

---
+++ src/com/demo/bean/User.java
@@ -0,0 +1,19 @@
+package com.demo.bean;
+
+public class User {
+	private String uname;
+	private String upass;
+	public String getUname() {
+		return uname;
+	}
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+	public String getUpass() {
+		return upass;
+	}
+	public void setUpass(String upass) {
+		this.upass = upass;
+	}
+
+}
